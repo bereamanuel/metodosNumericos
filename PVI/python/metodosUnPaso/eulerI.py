643f4a7afc6368b8d8230ab5f80bed3db9a85e94
Manuel Berea <manuel_berearellano@hotmail.com>
2022-09-22 21:19:46 +0200

    Euler

--- PVI/python/metodosUnPaso/eulerI.py
+++ PVI/python/metodosUnPaso/eulerI.py
@@ -1,4 +1,4 @@
-def eulerI(f,df,a,b,y0,n):
+def eulerI(f,a,b,y0,N,tol,maxiter):
     """
     Con esta función vamos a implementar el método de Euler implicito
     INPUT:
@@ -14,28 +14,40 @@ def eulerI(f,df,a,b,y0,n):
     """
     import numpy as np
 
-    h = (b-a)/n
-    t = np.insert(np.arange(a, b, h),n,b)
+    h = (b-a)/N
+    t = np.insert(np.arange(a, b, h),N,b)
 
     y = [y0]
+    for k in range(0,N):
+        x0 = y[k]
+        i = 1 
+        dif = tol+1
+        while i < maxiter and dif>tol:
+            fx0 = f(t[k+1],x0,"f")
+            df0 = f(t[k+1],x0,"df")
+            g = x0 - y[k] - h*fx0
+            dg = 1-h*dfx0
+            x1 = x0 - g/dg
+            dif = abs(x1-x0[0])
+            i = i + 1
+            x0 = x1
 
-    for k in range(1,n):
-        yk = 1
-        y.append(y[k-1] + h*f(t[k],yk))
+        y.append(y[k] + h *f(t[k+1],x0,"f") )
+
     return(t, y)
 
-def f(t,y):
-    return( (1-2*t)*y )
-
-def df(t,y):
-    return( (1-2*t) )
+def f(x,y,t):
+    if t == "f":
+        return((1-2*x)*y)
+    else:
+        return((1-2*x))
 
 a = 0
 b = 3
-y0 = 1
+y0 = [1]
 N = [2,4,8,16,32,64]
 
-print(eulerI(f,df,a,b,y0,4))
+print(eulerI(f,a,b,y0,4, tol = 10**(-6), maxiter= 50))
 
 # import matplotlib.pyplot as plt
 # for n in N:
